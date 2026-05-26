b5afc42948f1025c86439ab92b090adec490b6c1
Lucas Barragan <lucasbarragan000@gmail.com>
2026-05-26 22:23:20 +0000

    T20E-9: Generar reporte automatico de ventas

--- scripts/analisis_ventas.py
+++ scripts/analisis_ventas.py
@@ -46,3 +46,19 @@ print("Promedio de ventas:", round(promedio_ventas, 2))
 print("Venta máxima:", venta_maxima)
 
 print("Venta mínima:", venta_minima)
+
+
+reporte = open("resultados/reporte_ventas.txt", "w")
+
+reporte.write("ANÁLISIS DE VENTAS\n")
+reporte.write("-------------------\n\n")
+
+reporte.write(f"Total de ventas: {total_ventas}\n")
+reporte.write(f"Cantidad de ventas: {cantidad_ventas}\n")
+reporte.write(f"Promedio de ventas: {round(promedio_ventas, 2)}\n")
+reporte.write(f"Venta máxima: {venta_maxima}\n")
+reporte.write(f"Venta mínima: {venta_minima}\n")
+
+reporte.close()
+
+print("\nReporte generado correctamente.")
