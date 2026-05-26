3330489ed807616c6727b8238bbb249fcf454909
Lucas Barragan <lucasbarragan000@gmail.com>
2026-05-26 21:37:21 +0000

    T20E-7: Implementar lectura de datos CSV

--- scripts/analisis_ventas.py
+++ scripts/analisis_ventas.py
@@ -1,1 +1,21 @@
-print("Sistema de análisis de ventas")
+
+archivo = open("datos/sales_sample_2024.csv", "r")
+
+lineas = archivo.readlines()
+
+archivo.close()
+
+print("Primeros 5 registros:\n")
+
+for linea in lineas[1:6]:
+
+    datos = linea.strip().split(",")
+
+    id_venta = datos[0]
+    fecha = datos[1]
+    monto = datos[2]
+
+    print("ID:", id_venta)
+    print("Fecha:", fecha)
+    print("Monto:", monto)
+    print()
